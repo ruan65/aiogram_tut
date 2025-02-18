0d2f70d29967e05d744214efb87a36dfbb40c099
arudenko <arudenko@corp.finam.ru>
2025-02-18 10:22:30 +0300

    buttons

--- callback_handlers/actions_callback_handlers.py
+++ callback_handlers/actions_callback_handlers.py
@@ -4,6 +4,7 @@ from aiogram.types import CallbackQuery
 from keyboards.actions_keyboards import (
     actions_kb,
     rnd_num_updated_callback_data,
+    FixedRandomNumCallbackData,
 )
 
 router = Router(name=__name__)
@@ -15,4 +16,19 @@ async def handle_random_number_edited(callback_query: CallbackQuery):
     await callback_query.message.edit_text(
         text=f"Your random number: {randint(1, 101)}",
         reply_markup=actions_kb("generate"),
+    )
+
+
+@router.callback_query(FixedRandomNumCallbackData.filter())
+async def handle_random_number(
+    callback_query: CallbackQuery,
+    callback_data: FixedRandomNumCallbackData,
+):
+    await callback_query.answer(
+        text=(
+            f"Your fixed random number: {callback_data.number}\n"
+            f"Callback data: {callback_query.data!r}"
+        ),
+        show_alert=True,
+        cache_time=30,
     )
--- keyboards/actions_keyboards.py
+++ keyboards/actions_keyboards.py
@@ -1,12 +1,31 @@
+from random import randint
 from aiogram.types import (
     InlineKeyboardMarkup,
 )
+from aiogram.filters.callback_data import CallbackData
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 rnd_num_updated_callback_data = "rnd_num_updated_callback_data"
 
 
+class FixedRandomNumCallbackData(CallbackData, prefix="fixed-random-num"):
+    number: int
+
+
 def actions_kb(button_title) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
-    builder.button(text=button_title, callback_data=rnd_num_updated_callback_data)
+    builder.button(
+        text=button_title,
+        callback_data=rnd_num_updated_callback_data,
+    )
+    cb_data_1 = FixedRandomNumCallbackData(number=randint(1, 101))
+    builder.button(
+        text=f"Random number: {cb_data_1.number}",
+        callback_data=cb_data_1.pack(),
+    )
+    builder.button(
+        text="Random number: [HIDDEN]",
+        callback_data=FixedRandomNumCallbackData(number=randint(1, 101)).pack(),
+    )
+    builder.adjust(1)
     return builder.as_markup()
